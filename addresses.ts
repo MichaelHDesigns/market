5f2fa677e0f126d0ab13c8b048eb5475c29684ee
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-02-26 21:11:21 -0500

    Update addresses.ts

--- addresses.ts
+++ addresses.ts
@@ -1,2 +1,2 @@
 // Use your marketplace contract address here
-export const marketplaceContractAddress = '0xf7ba7cd986d5bC960433697Ca8cF9e7101F3c017';
+export const marketplaceContractAddress = '0x9e9cE7082E4B1308Cb2186EfD28ea0ABcFcF4187';
